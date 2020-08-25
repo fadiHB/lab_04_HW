278c087b43e02bce722b1f15e1b1db75c3d82352
fadiHB <eng.fadi.hb@gmail.com>
2020-08-26 01:06:32 +0300

    edit for javascript

---
+++ index.js
@@ -0,0 +1,26 @@
+
+var name ='Zaid Hendey';
+var age=30;
+
+
+console.log(' type of name ',typeof name);
+console.log(' type of age ',typeof age);
+
+
+
+if(age==30 &&name=='ahmad'){
+    alert('you are welcome');
+}else{
+    console.log(' stay here ');
+}
+
+
+var favmov;
+favmov = prompt(' what is your fav actres : ');
+console.log('user\'s fav mov', favmov);
+
+var favAct = confirm('nive to meet you');
+console.log('user fav actress' ,favAct);
+
+
+
