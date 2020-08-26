bf9c29c8a4f02d2582db1f5264d122d3ec47bd16
fadiHB <eng.fadi.hb@gmail.com>
2020-08-26 17:54:19 +0300

    THE FINAL ONE

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 
-var name ='Zaid Hendey';
-var age=30;
+function all() 
+{
 
+    var Sh = prompt( 'this is for lab04,05,06,07,08' )
+    var x  = prompt("")
 
-console.log(' type of name ',typeof name);
-console.log(' type of age ',typeof age);
+    var Nm = prompt(' what is your first neme please ? ');
 
+    console.log(Nm)
 
+    while (Nm !== "ahmad") {
+        Nm = prompt("No,You are not my TA,go away man,jut my TA can enter!")
+    }
 
-if(age==30 &&name=='ahmad'){
-    alert('you are welcome');
-}else{
-    console.log(' stay here ');
+    greeting1 = prompt("Welcome Mr. Mahasneh,gald you are here")
+
+    var NameAgain;
+    NameAgain = prompt(' tell me the true , whtat is your name ');
+    console.log('user\'s name again', NameAgain);
+
+    waring2 = prompt('you are welcome again');
+    if (NameAgain !== Nm) {
+        console.log(' stay here , How could you come here ! ');
+    }
+
+    var greeting2 = confirm('nice to meet you');
+    var OrderUser = prompt("how many images of apple do you want to print")
+
+    for (var count = 0; count < OrderUser; count++) {
+        document.write("<img src='image/ultimate-small-business-resource-180x180.png' alt=''>");
+    }
+
 }
 
-
-var favmov;
-favmov = prompt(' what is your fav actres : ');
-console.log('user\'s fav mov', favmov);
-
-var favAct = confirm('nive to meet you');
-console.log('user fav actress' ,favAct);
-
-
+all();
 
